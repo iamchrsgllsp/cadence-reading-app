e21ef676d258053e7f2abf59662ba475269152b4
iamchrsgllsp <iamchrsgllsp@outlook.com>
2025-11-30 22:43:37 +0000

    small fix

--- services/flask/routes.py
+++ services/flask/routes.py
@@ -252,7 +252,7 @@ def user_profile(user):
 
     print(f"Current Book: {currentbook}")
     print(f"TBR: {better_data}")
-    user = "wegotfight"
+
     return render_template(
         "userprofile.html",
         img=img,
