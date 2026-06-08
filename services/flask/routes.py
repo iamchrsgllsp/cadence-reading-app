f906d8e83d4f54dda243d3e3c6f290725a89f796
iamchrsgllsp <iamchrsgllsp@outlook.com>
2026-06-08 20:32:27 +0100

    add: updated version and updated icon and logo

--- services/flask/routes.py
+++ services/flask/routes.py
@@ -671,7 +671,7 @@ def app_version():
     data = {
         "version_code": 10,
         "download_url": "https://mpmblozcvymuwujwvefy.supabase.co/storage/v1/object/public/cadence_storage/cadence.apk",
-        "description": '✨ New Features are Live on Cadence! ✨\nChoose version when adding to library\nANDROID WIDGETS:SEE CURRENTLY READING STATS\n✅Social Interaction - Share books with your friends, chat and start group discussions (book clubs coming soon)\n✅ Total Library Overhaul – Your books have never looked better.\n✅ Role Badges – Earn your stripes.\n✅ Seamless Sign-In – Jump straight back into the action.\n✅ App Integration – Save titles on the go with "Add to Library."\n✅ The Full Shelf – A beautiful new way to browse.\n🏗️ Sneak Peek: The Medals system is currently in development!\nKeep your eyes peeled for more updates! 🚀',
+        "description": '✨ New Features are Live on Cadence! ✨\nNew icon: Cat icon created by Freepik - Flaticon\nChoose version when adding to library\nANDROID WIDGETS:SEE CURRENTLY READING STATS\n✅Social Interaction - Share books with your friends, chat and start group discussions (book clubs coming soon)\n✅ Total Library Overhaul – Your books have never looked better.\n✅ Role Badges – Earn your stripes.\n✅ Seamless Sign-In – Jump straight back into the action.\n✅ App Integration – Save titles on the go with "Add to Library."\n✅ The Full Shelf – A beautiful new way to browse.\n🏗️ Sneak Peek: The Medals system is currently in development!\nKeep your eyes peeled for more updates! 🚀',
     }
 
     return Response(
